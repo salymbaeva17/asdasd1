f00e761de7b6dc1ba096ef5a0688dec06c62cc47
Aitolkun Salymbaeva <salymbaeva17@gmail.com>
2021-07-23 21:56:20 +0600

    8 commit

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+let month = 4
+if ( month >= 3 && month <= 5 ){
+    console.log("Spring!")
+} else if( month >= 6 && month <= 8 ){
+    console.log("Summer!")
+} else if ( month >=9 && month <= 11 ){
+    console.log("Autumn!")
+} else if ( month === 1 || month === 2 || month === 12 ){
+    console.log("Winter!")
+} else {
+    console.log("NO MONTH!")
+}
+
 
 let a5 = 7
 let b3 = 5
