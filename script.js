3e7c534d140c5dc7af07618ce677b2489de33ec3
Aitolkun Salymbaeva <salymbaeva17@gmail.com>
2021-07-23 21:52:04 +0600

    2 commit

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
+
+
+
+let a = 10
+let b = 2
+if (a % b === 0) {
+    console.log("Четное число")
+} else {
+    console.log("Нечетное число")
+}
+
+
 let number = 45
-if ( number % 2 === 0 ){
+if (number % 2 === 0) {
     console.log("Четное число")
 } else {
     console.log("Нечетное число")
