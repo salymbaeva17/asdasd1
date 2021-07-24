e0b83bf9862a0c09109bedf4db8ff68268028597
Aitolkun Salymbaeva <salymbaeva17@gmail.com>
2021-07-24 12:21:08 +0600

    12 commit

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+let time = 16
+if ( time >=0 && time <=15){
+    console.log("Первая четверть")
+} else if( time >=15 && time <=30 ){
+    console.log("Вторая четверть")
+} else if ( time >= 30 && time <= 45 ){
+    console.log("Третья четверть")
+} else if ( time >=45 && time <= 60 ){
+    console.log("Четвёртая четверть")
+} else {
+    console.log("Неверное число")
+}
+
+
 let age = 95
 if ( age >= 10 && age <=20 ){
     console.log(`Мне ${age} лет`)
